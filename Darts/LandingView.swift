ec847a320abb8063526c0eb970ab0496c906297a
Joe <jgarnett370@gmail.com>
2024-03-22 18:34:59 +0000

    add basic landing page layout

---
+++ Darts/LandingView.swift
@@ -0,0 +1,18 @@
+//
+//  LandingView.swift
+//  Darts
+//
+//  Created by Joseph Garnett on 22/03/2024.
+//
+
+import SwiftUI
+
+struct LandingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandingView()
+}
