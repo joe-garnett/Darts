e328398189f50240b862f38c0152f641e0421440
Joe <jgarnett370@gmail.com>
2024-03-22 17:55:16 +0000

    Initial Commit

---
+++ Darts/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Darts
+//
+//  Created by Joseph Garnett on 22/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Darts/DartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DartsApp.swift
+//  Darts
+//
+//  Created by Joseph Garnett on 22/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct DartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
